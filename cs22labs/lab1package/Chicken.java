2290915676af33c610ced66926286e1c51cceb16
Darah Via <ddmoscoso@up.edu.ph>
2023-09-11 20:52:10 +0800

    lab 1

--- cs22labs/lab1package/Chicken.java
+++ cs22labs/lab1package/Chicken.java
@@ -2,7 +2,7 @@ package lab1package;
 
 public class Chicken{
     public static int NUMBER_OF_CHICKEN;
-    String name;
+    public String name;
     public Chicken(){
         NUMBER_OF_CHICKEN++;
     }
