117a365730fb3f6b9023884c8cb34d19714b216a
darahvia <126293709+darahvia@users.noreply.github.com>
2023-10-24 09:56:49 +0800

    Add files via upload

---
+++ lab5package/CoffeeCappuccino.java
@@ -0,0 +1,11 @@
+package lab5package;
+
+public class CoffeeCappuccino extends Coffee {
+    public CoffeeCappuccino() {
+        name = "cappuccino";
+        waterCost = 200;
+        milkCost = 100;
+        beansCost = 12;
+        moneyCost = 3.0;
+    }
+}
---
+++ lab5package/CoffeeLatte.java
@@ -0,0 +1,11 @@
+package lab5package;
+
+public class CoffeeLatte extends Coffee {
+    public CoffeeLatte() {
+        name = "latte";
+        waterCost = 350;
+        milkCost = 75;
+        beansCost = 20;
+        moneyCost = 2.5;
+    }
+}
